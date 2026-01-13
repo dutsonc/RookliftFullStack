568e792be8c094b38e1f5f454dab6f147ec6b8c9
dutsonc <dut20001@byui.edu>
2026-01-13 11:05:11 -0600

    fixed syntax in index and database

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,11 +15,11 @@ const startServer = async () => {
 
         app.listen(process.env.PORT || 8000, () => {
             console.log(`Server is running on port:
-                 ${process.env.PORT}`); }
+                 ${process.env.PORT}`);
+        });
     } catch (error) {
         console.error('Failed to start server:', error);
-    
-}
+    }
 }
 
     startServer();
